6bb996d07e44ff94336058a02e93c4e8922804ad
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-11-14 15:04:57 -0300

    Streamlit: Criação do Menu em selectbox

--- front-end/app.py
+++ front-end/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/"
+API_URL = "http://127.0.0.1:8000"
+
+st.set_page_config(page_title="Gerência de Produtos", layout="wide")
+st.title("® Hype Almoxarifado")
+
+menu = st.sidebar.selectbox(
+    "Menu",
+    ["Listar Produtos", "Cadastrar Produto", "Atualizar Produto", "Deletar Produto"]
+)
