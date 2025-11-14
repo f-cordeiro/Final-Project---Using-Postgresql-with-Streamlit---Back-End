12becd3f603247eadaf5d5d6b474627d4a3cdd7a
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-11-14 15:08:38 -0300

    Streamlit: Atualizar Produtos

--- front-end/app.py
+++ front-end/app.py
@@ -53,3 +53,24 @@ elif menu == "Cadastrar Produto":
                 st.error("Erro ao cadastrar produto")
         else:
             st.warning("Preencha todos os campos obrigatórios")
+
+elif menu == "Atualizar Produto":
+    st.subheader("Atualizar Produto em Estoque ♻")
+
+    id_produto = st.number_input("ID do Produto", min_value=1, step=1)
+    novo_preco = st.number_input("Novo Preço", min_value=0.0, format="%.2f")
+    nova_quantidade = st.number_input("Nova Quantidade", min_value=0, step=1)
+
+    if st.button("Atualizar Produto ♻"):
+        params = {
+            "novo_preco": novo_preco,
+            "nova_quantidade": nova_quantidade,
+        }
+        
+        response = requests.put(f"{API_URL}/produtos/{id_produto}", params=params)
+
+        if response.status_code == 200:
+            msg = response.json()
+            st.success(msg.get("mensagem", "Produto atualizado!"))
+        else:
+            st.error("Erro ao atualizar o produto")
