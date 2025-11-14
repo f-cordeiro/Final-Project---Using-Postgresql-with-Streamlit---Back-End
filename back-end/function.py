91b0c8188bb83e5cb099722e91c47deabb5568fb
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-11-14 09:45:19 -0300

    function de Cadastrar Produtos ✔

--- back-end/function.py
+++ back-end/function.py
@@ -19,3 +19,19 @@ def criar_tabela():
         finally:
             cursor.close()
             conexao.commit()
+
+
+def cadastrar_produto(name_produto, category_produto, price_produto, amount_produto):
+    conexao, cursor = conector()
+    if conexao:
+        try:
+            cursor.execute(
+                "INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES (%s, %s, %s, %s)",
+                (name_produto, category_produto, price_produto, amount_produto)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao tentar cadastrar o Produto: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
