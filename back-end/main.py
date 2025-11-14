2d0d9e4a662fc9f446026aa22c65a89cb3f6de72
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-11-14 10:26:15 -0300

    FastAPI : def listar_produtos ✔

--- back-end/main.py
+++ back-end/main.py
@@ -11,3 +11,19 @@ def home():
 def criar_produto(nome: str, categoria: str, preco: float, quantidade: int):
     function.cadastrar_produto(nome, categoria, preco, quantidade)
     return {"mensagem": "Produto cadastrado com sucesso!"}
+
+@app.get("/produtos")
+def listar_produtos():
+    produtos = function.listar_produto()
+    lista = []
+
+    for linha in produtos:
+        lista.append({
+            "id": linha[0],
+            "nome": linha[1],
+            "categoria": linha[2],
+            "preco": linha[3],
+            "quantidade": linha[4]
+        })
+    
+    return {"produtos": lista}
