472567e8ad64cf2a33ba48e8e491046a69f46c9b
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-11-14 10:21:57 -0300

    FastAPI : def criar_produto ✔

--- back-end/main.py
+++ back-end/main.py
@@ -6,3 +6,8 @@ app = FastAPI(title="Gerência de Produtos")
 @app.get("/")
 def home():
     return {"mensagem": "Bem-vindo á gerencia de produtos"}
+
+@app.post("/produtos")
+def criar_produto(nome: str, categoria: str, preco: float, quantidade: int):
+    function.cadastrar_produto(nome, categoria, preco, quantidade)
+    return {"mensagem": "Produto cadastrado com sucesso!"}
