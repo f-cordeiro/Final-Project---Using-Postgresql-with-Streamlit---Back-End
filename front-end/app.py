098d840af87daccac10cc42e0e4cd33b84378536
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-11-14 15:07:22 -0300

    Streamlit: Cadastrar Produtos

--- front-end/app.py
+++ front-end/app.py
@@ -26,3 +26,30 @@ if menu == "Listar Produtos":
             st.info("Nenhum produto cadastrado ainda!, Tente Novamente !")
     else:
         st.error("Erro ao conectar com a API.")
+
+elif menu == "Cadastrar Produto":
+    st.header("Bem - Vindo a Área de Cadastro de Produtos!")
+    st.subheader("✔ Cadastrar Novo Produto")
+
+    nome= st.text_input("Nome")
+    categoria = st.text_input("Categoria")
+    preco = st.number_input("Preço", min_value=0.0, format="%.2f")
+    quantidade = st.number_input("Quantidade", min_value=0, step=1)
+
+    if st.button("Cadastrar Produto ✅"):
+        if nome and categoria:
+            params = {
+                "nome": nome,
+                "categoria": categoria,
+                "preco": preco,
+                "quantidade": quantidade,
+            }
+
+            response= requests.post(f"{API_URL}/produtos", params=params)
+
+            if response.status_code == 200:
+                st.success("Produto cadastrado com sucesso!")
+            else:
+                st.error("Erro ao cadastrar produto")
+        else:
+            st.warning("Preencha todos os campos obrigatórios")
